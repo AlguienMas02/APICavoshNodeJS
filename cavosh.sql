794533727551c2ead0f4b673c8c51610ff32fdc2
AlguienMas020 <mpaulinodurand2015@gmail.com>
2025-11-09 19:10:52 -0500

    feat: :sparkles: Script SQL

---
+++ cavosh.sql
@@ -0,0 +1,124 @@
+
+--
+-- Base de datos: `cavosh`
+--
+create database cavosh;
+use cavosh;
+
+--
+-- Estructura de tabla para la tabla `extras`
+--
+
+CREATE TABLE `extras` (
+  `id` int(11) NOT NULL,
+  `nombre` int(35) NOT NULL,
+  `precio` decimal(6,2) NOT NULL,
+  `producto_id` int(11) NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(35) NOT NULL,
+  `precio` decimal(6,2) NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tienda`
+--
+
+CREATE TABLE `tienda` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `telefono` int(9) NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(40) NOT NULL,
+  `contrasena` varchar(20) NOT NULL
+);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `extras`
+--
+ALTER TABLE `extras`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `llavef` (`producto_id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tienda`
+--
+ALTER TABLE `tienda`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `extras`
+--
+ALTER TABLE `extras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tienda`
+--
+ALTER TABLE `tienda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `extras`
+--
+ALTER TABLE `extras`
+  ADD CONSTRAINT `llavef` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`);
+COMMIT;
+
